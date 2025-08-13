f23a5e0ccc6f847a1e7547287e959c28a5f8a03c
salahe03 <s.moustafid@aui.ma>
2025-08-13 22:25:08 +0100

    add allocation routes to backend

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -5,17 +5,28 @@ const {
   createExpense,
   updateExpense,
   deleteExpense,
-  getExpenseStats
+  getExpenseStats,
+  allocateExpense,
+  deleteAllocation,
+  getFinanceOverview
 } = require('../controllers/expenseController');
 const { authenticateToken, requireAdmin } = require('../middleware/authMiddleware');
 
 router.use(authenticateToken);
 router.use(requireAdmin);
 
+// Stats + finance overview
 router.get('/stats', getExpenseStats);
+router.get('/overview', getFinanceOverview);
+
+// CRUD
 router.get('/', getExpenses);
 router.post('/', createExpense);
 router.put('/:id', updateExpense);
 router.delete('/:id', deleteExpense);
+
+// Allocations
+router.post('/:id/allocate', allocateExpense);
+router.delete('/:id/allocations/:allocId', deleteAllocation);
 
 module.exports = router;
